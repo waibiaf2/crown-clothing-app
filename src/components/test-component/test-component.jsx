f4ace6564ae517c8c54cfccae7c947c4a540de79
Waibi Andrew Franklin <waibiaf2@gmail.com>
2023-08-17 22:15:29 +0300

    Adds routing to application

---
+++ src/components/test-component/test-component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestComponent = () => {
+    return (
+        <div>
+            <h1>This is a test component</h1>
+        </div>
+    );
+};
+
+export default TestComponent;
