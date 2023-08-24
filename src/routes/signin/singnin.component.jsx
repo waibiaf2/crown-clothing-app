9420e3a96bc2ceeb042b12db291ec755131b8485
Waibi Andrew Franklin <waibiaf2@gmail.com>
2023-08-24 19:46:27 +0300

    Setup auth using firebase redirect

--- src/routes/signin/singnin.component.jsx
+++ src/routes/signin/singnin.component.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getRedirectResult} from 'firebase/auth'
 import {
+    auth,
     signInWithGooglePopup,
+    signInWithGoogleRedirect,
     createUserDocumentFromAuth
     } from './../../utils/firebase/firebase.utils'
 
 const SingninComponent = () => {
+    const creatUser = async () => {
+        const response = await getRedirectResult(auth);
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+            console.log(response.user);
+        }
+        return () => {
+        
+        };
+    }
+    
+    useEffect(() => {
+        creatUser();
+    }, []);
+    
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const {userDocRef} = await createUserDocumentFromAuth(user);
-        
         console.log(user);
     }
     
@@ -17,6 +34,9 @@ const SingninComponent = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sing in with Google
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sing in with Google Redirect
             </button>
         </div>
     );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -11,9 +11,6 @@ import {
     setDoc } from 'firebase/firestore'
 
 
-//Access token
-//eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzODBlZjEyZjk1ZjkxNmNhZDdhNGNlMzg4ZDJjMmMzYzIzMDJmZGUiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiQW5kcmV3IEZyYW5rbGluIFdhaWJpIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBY0hUdGVNeFNNMVBtTVhKOTFpaUVIZ2VNUkR0SVlhSXo5Rk5GalhyQnRiWXRXNj1zOTYtYyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9jcm93bi1jbG90aGluZy1kYi00MjdiOCIsImF1ZCI6ImNyb3duLWNsb3RoaW5nLWRiLTQyN2I4IiwiYXV0aF90aW1lIjoxNjkyNzY0NzE2LCJ1c2VyX2lkIjoiTTA5dmhaRUg2b1RuNWZlZTZlckkxVjZicVJ4MSIsInN1YiI6Ik0wOXZoWkVINm9UbjVmZWU2ZXJJMVY2YnFSeDEiLCJpYXQiOjE2OTI3NjQ3MTYsImV4cCI6MTY5Mjc2ODMxNiwiZW1haWwiOiJ3YWliaWFmNUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjEwMTk3NjgxMDYyNDY3MjQ0NzE0MCJdLCJlbWFpbCI6WyJ3YWliaWFmNUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.Ho4bhcrYr40N4oJn7qMGDacGi_b2Gjqj9zhYzuT6mx3exPKZSCpu2GVyqnl7EmXzwDNK9tlubf3tp8zucvcGbUkUwK4YXiQFr0EUYptpkRvHIm2m-ukHCNZzdaDEpsmNsAhDhjLeErOCdyV-o9Ez9kJISZpmwMlnikbaiAjF2BwdE-NOnay6kOgy_rFz7fdfW0IvyPieSlqK2K6eUD4JivcShdza9a4_jmaiKjg9VEI0SlKec495PCuW7mlzO19kMkbobgu-zEcALkevi93GeQysa5KWY79iBm9moMWYyVpQIMXuxKy2teoZdsqPE8x_wllrzV1VzYYxUCWG4gRohw
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDDfnyDKf1FqG4hkFVUWfMitrfCx0kJMcc",
@@ -24,16 +21,21 @@ const firebaseConfig = {
     appId: "1:250639586687:web:e75e9a910497985174f64a"
 };
 
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
     prompt: "select_account"
 });
-
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
 
 export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
