6513fa347b154ab8c5d56928c580c95ea19c8738
Waibi Andrew Franlin <waibiaf2@gmail.com>
2023-09-29 22:13:48 +0300

    Coverted cart dropdown to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import './cart-dropdown.styles.scss';
+// import './cart-dropdown.styles.scss';
 import ButtonComponent from '../button/button.component';
 import CartItemComponent from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
+import {CartDropDownContainer, CartItems, EmptyMessage} from "./cart-dropdown.styles";
 
 const CartDropdownComponent = () => {
 	const {cartItems} = useContext(CartContext);
@@ -12,14 +13,17 @@ const CartDropdownComponent = () => {
 
 	const goToCheckoutHandler = () => navigate('/checkout');
 	return (
-		<div className="cart-dropdown-container">
-			<div className="cart-items">
-				{cartItems.map(item => <CartItemComponent key={item.id} cartItem={item}/>)}
-			</div>
+		<CartDropDownContainer>
+			<CartItems>
+				{cartItems.length ?
+					(cartItems.map(item => <CartItemComponent key={item.id} cartItem={item}/>)):
+					(<EmptyMessage>Your cart is empty</EmptyMessage>)
+				}
+			</CartItems>
 			<ButtonComponent onClick={goToCheckoutHandler}>
 				Go to checkout
 			</ButtonComponent>
-		</div>
+		</CartDropDownContainer>
 	);
 };
 
