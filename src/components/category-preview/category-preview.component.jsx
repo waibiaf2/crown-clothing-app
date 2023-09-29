b9bb7d7d58769307d76870e58355edfb6f7d8607
Waibi Andrew Franlin <waibiaf2@gmail.com>
2023-09-29 11:11:24 +0300

    Adds category-preview component

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './category-preview.styles.scss';
+
+import ProductCard from "../product-card/product-card";
+
+const CategoryPreviewComponent = ({title, products}) => {
+    return (
+        <div className="category-preview-container">
+            <h2 className="title">
+                <span>{title.toUpperCase()}</span>
+            </h2>
+            <div className="preview">
+                {
+                    products
+                        .filter((_, idx) => idx < 4)
+                        .map( (product) => <ProductCard key={product.id} product={product}/>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default CategoryPreviewComponent;
