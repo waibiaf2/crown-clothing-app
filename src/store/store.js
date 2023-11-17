fcfd076852b400c9998ec6ef4466bd82e0e9c1fe
Waibi Andrew Franklin <waibiaf2@gmail.com>
2023-11-17 21:10:36 +0300

    optimize middleware to run only in development enviroment

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,13 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 import {persistStore, persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import logger from 'redux-logger'
 
 // import logger from 'redux-logger';
 
 import {rootReducer} from "./root-reducer";
 
-const loggerMiddleware  = (store) => (next) => (action) => {
-	if(!action.type) {
-		return next(action);
-	}
-	
-	console.log('type:', action.type);
-	console.log('payload', action.payload);
-	console.log('currentState:', store.getState());
-	
-	next(action);
-	console.log('next state: ', store.getState());
-}
+
 
 const persistConfig = {
 	key: 'root',
@@ -27,7 +17,9 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+	Boolean
+);
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
